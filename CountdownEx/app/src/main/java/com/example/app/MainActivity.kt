1561b24f0c385c8d2a8fc05c2672c94214d6e975
Youngdeok Hwang <medusakiller@gmail.com>
2020-06-05 03:31:31 +1000

    test with CompletableJob and CoroutineScope

--- CountdownEx/app/src/main/java/com/example/app/MainActivity.kt
+++ CountdownEx/app/src/main/java/com/example/app/MainActivity.kt
@@ -1,30 +1,87 @@
 package com.example.app
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.content_main.*
 import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "COUNTDOWN"
+    private lateinit var job: CompletableJob
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (!::job.isInitialized) {
+            initJob()
+        }
         countTask(tv_count, btn_start, btn_stop)
     }
+
+    private fun initJob() {
+        job = Job()
+        job.invokeOnCompletion {
+            it?.message.let {
+                var msg = it
+                if(msg.isNullOrBlank()) {
+                    msg = "Unknown error."
+                }
+                Log.e(TAG, "$job was cancelled: $msg")
+            }
+            tv_count.text = "Done!"
+        }
+        tv_count.text = "Ready"
+    }
+
+    private fun countTask(count: TextView, start: Button, stop: Button) {
+        // UI 컨텍스트에서 코루틴 실행
+        /*val job = GlobalScope.launch(Dispatchers.Main, start = CoroutineStart.LAZY) {
+            for (i in 10 downTo 1) { // 카운트다운
+                count.text = "Countdown $i ..." // UI 업데이트
+                delay(1000)
+            }
+            count.text = "Done!"
+        }*/
+        start.setOnClickListener {
+            if (count.text.contains("Countdown")) {
+                Log.d(TAG, "$job is already active.")
+                resetJob()
+            } else {
+                CoroutineScope(Main + job).launch {
+                    Log.d(TAG, "$this coroutine is activated with job $job")
+                    for (i in 10 downTo 1) { // 카운트다운
+                        count.text = "Countdown $i ..." // UI 업데이트
+                        delay(1000)
+                    }
+                    count.text = "Count Done!"
+                }
+            }
+            //job.start()
+        }
+        stop.setOnClickListener {
+            resetJob()
+        }
+    }
+
+    private fun resetJob() {
+        if(job.isActive || job.isCompleted){
+            job.cancel(CancellationException("Resetting job"))
+        }
+        initJob()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+    }
+
 }
 
-fun countTask(count: TextView, start: Button, stop: Button) {
-    // UI 컨텍스트에서 코루틴 실행
-    val job = GlobalScope.launch(Dispatchers.Main, start = CoroutineStart.LAZY) {
-        for (i in 10 downTo 1) { // 카운트다운
-            count.text = "Countdown $i ..." // UI 업데이트
-            delay(1000)
-        }
-        count.text = "Done!"
-    }
-    start.setOnClickListener { job.start() }
-    stop.setOnClickListener { job.cancel() }
-}
